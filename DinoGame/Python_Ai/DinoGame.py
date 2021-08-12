25b7725c3bbf6e8064a464c439ef2a9106a51673
Sean-Mabli <smdrone1@gmail.com>
2021-08-12 09:04:46 -0400

    transition to ai

--- DinoGame/Python_Ai/DinoGame.py
+++ DinoGame/Python_Ai/DinoGame.py
@@ -1,7 +1,6 @@
 import pygame
 import numpy as np
 from NEAT import NEAT
-import time
 
 InToHid1 = NEAT(4, 6, MutationRate=0.1, PopulationSize=1000)
 Hid1ToOut = NEAT(6, 3, MutationRate=0.1, PopulationSize=1000)
@@ -12,10 +11,10 @@ pygame.display.set_caption('Dinosaur Game')
 DisplayShape = (960, 540)
 Display = pygame.display.set_mode([DisplayShape[0], DisplayShape[1]])
 
-Bird = pygame.image.load('DinoGame\Python_Ai\data\Bird002.png')
-Cactus = pygame.image.load('DinoGame\Python_Ai\data\Cactus001.png')
-DinoDuck = pygame.image.load('DinoGame\Python_Ai\data\DinoDuck004.png')
-DinoWalk = pygame.image.load('DinoGame\Python_Ai\data\DinoWalk002.png')
+Bird = pygame.image.load('DinoGame/Python_Ai/data/Bird002.png')
+Cactus = pygame.image.load('DinoGame/Python_Ai/data/Cactus001.png')
+DinoDuck = pygame.image.load('DinoGame/Python_Ai/data/DinoDuck004.png')
+DinoWalk = pygame.image.load('DinoGame/Python_Ai/data/DinoWalk002.png')
 
 Alive = np.array([True] * 1000, dtype=bool)
 HighScore = 0
@@ -28,22 +27,23 @@ Speed = 8 # Pixels per loop
 Road = np.stack((np.random.randint(0, DisplayShape[0], 100), np.random.randint(DisplayShape[1] - 20, DisplayShape[0], 100)))
 
 ObjectShape = (73, 47)
-Object = np.array([[1000, 1400, 1800], [DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20), DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20), DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20)]])
+Object = np.array([[400, 800, 1200], [DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20), DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20), DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20)]])
 ObjectType = np.array(['Cactus', 'Cactus', 'Cactus'])
 
 DinoShape = (40, 43)
+DinoWalkShape = (40, 43)
+DinoDuckShape = (55, 26)
 Dino = np.array([20, DisplayShape[1] - DinoShape[1]])
 DinoType = 'Walk'
 
 WHITE = (255, 255, 255)
 GRAY = (83, 83, 83)
 
-for Generation in range(100):
+for Generation in range(20):
   while np.sum(Alive) != 0:
     Display.fill(WHITE)
-    Display.blit(pygame.font.SysFont("Raleway", 40).render("Score: " + str(Score), 1, GRAY), (750, 10))
-    Display.blit(pygame.font.SysFont("Raleway", 40).render("High Score: " + str(HighScore), 1, GRAY), (750, 40))
-    time.sleep(0.01)
+    # Display.blit(pygame.font.SysFont("Raleway", 40).render("Score: " + str(Score), 1, GRAY), (750, 10))
+    # Display.blit(pygame.font.SysFont("Raleway", 40).render("High Score: " + str(HighScore), 1, GRAY), (750, 40))
 
     # Road
     pygame.draw.line(Display, GRAY, (0, DisplayShape[1] - 20), (DisplayShape[0], DisplayShape[1] - 20))
@@ -72,8 +72,8 @@ for Generation in range(100):
       Object[0, 2] = np.random.randint(1000, 1400)
       while abs(Object[0, 0] - Object[0, 1]) < 400 or abs(Object[0, 1] - Object[0, 2]) < 400 or abs(Object[0, 0] - Object[0, 2]) < 400:
         Object[0, 2] = np.random.randint(1000, 1400)
-      if np.random.random_sample() <= 0.2:
-        Object[1, 2] = DisplayShape[1] - (ObjectShape[1] + DinoShape[1]) + np.random.randint(0, 10)
+      if np.random.random_sample() <= 0.25:
+        Object[1, 2] = DisplayShape[1] - (ObjectShape[1] + DinoWalkShape[1]) + np.random.randint(0, 10)
         ObjectType[i] = 'Bird'
       else:
         Object[1, 2] = DisplayShape[1] - ObjectShape[1] - np.random.randint(0, 20)
@@ -84,10 +84,10 @@ for Generation in range(100):
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP and Dino[1] == DisplayShape[1] - DinoShape[1]:
           Velocity = - 10
-          DinoShape = (40, 43)
+          DinoShape = DinoWalkShape
           DinoType = 'Walk'
         if event.key == pygame.K_DOWN and Dino[1] == DisplayShape[1] - DinoShape[1]:
-          DinoShape = (55, 26)
+          DinoShape = DinoDuckShape
           DinoType = 'Duck'
 
     Dino[1] += Velocity
@@ -103,10 +103,10 @@ for Generation in range(100):
     for i in range(len(Object[0])):
       if Dino[0] < Object[0, i] + ObjectShape[0] and Dino[0] + DinoShape[0] > Object[0, i] and Dino[1] < Object[1, i] + ObjectShape[1] and Dino[1] + DinoShape[1] > Object[1, i]:
         Alive = False
-        HighScore = max(HighScore, Score)
+        # HighScore = max(HighScore, Score)
     if Dino[1] == 0:
       Alive = False
-      HighScore = max(HighScore, Score)
+      # HighScore = max(HighScore, Score)
 
     pygame.display.update()
 
--- DinoGame/Python_Ai/LearnAndMutate.pde
+++
@@ -1,8 +0,0 @@
-void LearnAndMutate() {
-    // Reset Varaibles 
-    for (int i = 1; i < Object.length + 1; i++) {
-      Object[i - 1] = new PVector(int(random(height - 67, height - 47)), i * (2000 / Object.length));
-      ObjectType[i - 1] = "Cactus";
-    }
-  }
-}
